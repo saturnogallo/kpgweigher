3d394694d054031142235f801347b09d0ba7e699
fdlxh@hotmail.com <fdlxh@hotmail.com@83333240-a642-11dd-a8c6-677c70d10fe4>
2008-10-30 13:46:24 +0000

    Removed file/folder

--- trunk/Init.h
+++
@@ -1,6 +0,0 @@
-// Initialization header files
-extern void Init_Port(void);
-extern void Init_Timers(void);
-extern void Enable_XMEM(void);
-extern void Init_Ex_Interrupt(void);
-extern void Init_ADC(void);
